05921348ce8b3d0e1d1aa4cef14a3860b5c09bc6
Александр <palenko.as@mail.ru>
2022-03-18 21:37:28 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #276067]

--- src/main/java/ru/job4j/transfer/UserStorage.java
+++ src/main/java/ru/job4j/transfer/UserStorage.java
@@ -2,30 +2,47 @@ package ru.job4j.transfer;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 @ThreadSafe
 public final class UserStorage {
 
-    private final HashSet<User> users = new HashSet<>();
+    private final Map<Integer, User> users = new HashMap<>();
 
-    boolean add(User user) {
-        return users.add(user);
-    }
-
-    boolean update(User user) {
+    synchronized boolean add(User user) {
+        boolean rsl = true;
+        if (!users.containsValue(user)) {
+            add(user);
+            return rsl;
+        }
         return false;
     }
 
-    boolean delete(User user) {
-        return users.remove(user);
+    synchronized boolean update(User user) {
+        boolean rsl = true;
+        if (users.containsValue(user)) {
+            delete(user);
+            update(user);
+            return rsl;
+        }
+        return false;
+    }
+
+    synchronized boolean delete(User user) {
+        boolean rsl = true;
+        if (users.containsKey(user.getId())) {
+            delete(user);
+            return rsl;
+        }
+        return false;
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        User userFrom = users.stream().filter(u -> fromId == u.getId()).findFirst().orElse(null);
-        User userTo = users.stream().filter(u -> toId == u.getId()).findFirst().orElse(null);
+        User userFrom = users.values().stream().filter(u -> fromId == u.getId()).findFirst().orElse(null);
+        User userTo = users.values().stream().filter(u -> toId == u.getId()).findFirst().orElse(null);
         if (userFrom != null && userTo != null) {
-           /* userFrom.setAmount(userFrom.getAmount() - amount); */
+            /* userFrom.setAmount(userFrom.getAmount() - amount); */
             userTo.setAmount(userTo.getAmount() + amount);
         }
     }
