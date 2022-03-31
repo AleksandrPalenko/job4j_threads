4c932f2a3c03807745b0086cfb9c004a3e880518
Александр <palenko.as@mail.ru>
2022-03-31 16:46:16 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/delivery/EmailNotification.java
@@ -0,0 +1,37 @@
+package ru.job4j.delivery;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        pool.submit(() ->
+                new StringBuilder()
+                        .append("subject = ")
+                        .append(user.getEmail())
+                        .append("body = ")
+                        .append(user.getUserName()));
+    }
+
+    public synchronized void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+}
