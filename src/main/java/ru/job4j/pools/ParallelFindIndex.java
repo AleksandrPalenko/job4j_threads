a2cfca64a214051526090343d40cdd3cc06c40d9
Александр <palenko.as@mail.ru>
2022-04-01 18:06:29 +0300

    3. ForkJoinPool [#315067 #283051]

--- src/main/java/ru/job4j/pools/ParallelFindIndex.java
+++ src/main/java/ru/job4j/pools/ParallelFindIndex.java
@@ -4,25 +4,26 @@ package ru.job4j.pools;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelFindIndex extends RecursiveTask<Integer> {
+public class ParallelFindIndex<T> extends RecursiveTask<Integer> {
 
-    private final String[] array;
-    private final String obj;
+    private final T[] array;
+    private final T obj;
     private final int from;
     private final int to;
 
-    public ParallelFindIndex(String[] array, String obj, int from, int to) {
+    public ParallelFindIndex(T[] array, T obj, int from, int to) {
         this.array = array;
         this.obj = obj;
         this.from = from;
         this.to = to;
     }
 
-    public static int find(String[] object, String obj, int from, int to) {
+    public static <T> int find(T[] array, T obj, int from, int to) {
         int idx = -1;
-        for (int i = from; from <= to; i++) {
-            if (obj.equals(object[i])) {
+        for (int i = from; i <= to; i++) {
+            if (obj.equals(array[i])) {
                 idx = i;
+                break;
             }
         }
         return idx;
@@ -30,23 +31,22 @@ public class ParallelFindIndex extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-        int rsl = 0;
-        if (array.length <= 10) {
+        if ((from - to) <= 10) {
             return find(array, obj, from, to);
         }
         int mid = (from + to) / 2;
-        ParallelFindIndex leftFind = new ParallelFindIndex(array, obj, from, to);
-        ParallelFindIndex rightFind = new ParallelFindIndex(array, obj, mid + 1, to);
+        ParallelFindIndex<T> leftFind = new ParallelFindIndex<>(array, obj, from, to);
+        ParallelFindIndex<T> rightFind = new ParallelFindIndex<>(array, obj, mid + 1, to);
         leftFind.fork();
         rightFind.fork();
-        Integer left = leftFind.join();
-        Integer right = rightFind.join();
-        return rsl;
+        Integer left = (Integer) leftFind.join();
+        Integer right = (Integer) rightFind.join();
+        return Math.max(left, right);
     }
 
-    public static int findIndex(String[] object, String val) {
+    public static <T> int findIndex(T[] array, T value) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ParallelFindIndex(object, val, 0, object.length - 1));
+        return forkJoinPool.invoke(new ParallelFindIndex<>(array, value, 0, array.length - 1));
     }
 
 }
---
+++ src/test/java/ru/job4j/pools/ParallelFindIndexTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.pools;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ParallelFindIndexTest {
+
+    @Test
+    public void whenFinderIsTrue() {
+        String[] obj = {"Petr", "Ivan", "Olga"};
+        String el = "Olga";
+        int expected = 2;
+        int rsl = ParallelFindIndex.findIndex(obj, el);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenFinderIsMoreObjectsAndString() {
+        String[] str = new String[100];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = i + "a";
+        }
+        String el = "Olga";
+        int expected = -1;
+        int rsl = ParallelFindIndex.findIndex(str, el);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenFinderIsMoreObjectsAndInt() {
+        Integer[] obj = new Integer[100];
+        for (int i = 0; i < obj.length; i++) {
+            obj[i] = i + 1;
+        }
+        int rsl = ParallelFindIndex.findIndex(obj, 50);
+        assertThat(rsl, is(49));
+    }
+}
